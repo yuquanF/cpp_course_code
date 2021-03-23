65e876b9a15528cfae4b10f781ea0d29e1ae9231
yuquanf <420125423@qq.com>
2021-03-23 20:57:51 +0800

    init： 初始化项目

---
+++ chapter_2/eg_2_1/sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y, sum;
+    cout << "Please input two integers: " << endl;
+    cin >> x;
+    cin >> y;
+    sum = x + y;
+    cout << "x+y=" << sum << endl;
+
+    return 0;
+}
