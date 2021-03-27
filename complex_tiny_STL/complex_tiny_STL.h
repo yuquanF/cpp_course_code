1364df187d2db8dcec72088f8060028d37580665
yuquanf <420125423@qq.com>
2021-03-27 09:22:33 +0800

    refactor：去除多余空格

--- complex_tiny_STL/complex_tiny_STL.h
+++ complex_tiny_STL/complex_tiny_STL.h
@@ -13,54 +13,46 @@
 class Complex;
 
 Complex &_doapl(Complex *ths, const Complex &c); // +=
-
 Complex &_doami(Complex *ths, const Complex &c); // -=
-
 Complex &_doaml(Complex *ths, const Complex &c); // *=
 
-class Complex {
+class Complex
+{
 public:
     Complex(double r = 0, double i = 0) : re(r), im(i) {}
-
     Complex &operator+=(const Complex &);
-
     Complex &operator-=(const Complex &);
-
     Complex &operator*=(const Complex &);
-
-
     double real() const { return re; }
-
     double imag() const { return im; }
-
 
 private:
     double re, im;
-
     friend Complex &_doapl(Complex *ths, const Complex &c);
-
     friend Complex &_doami(Complex *ths, const Complex &c);
-
     friend Complex &_doaml(Complex *ths, const Complex &c);
 };
 
 // 实现辅助计算的友元函数
 inline Complex &
-_doapl(Complex *ths, const Complex &c) {
+_doapl(Complex *ths, const Complex &c)
+{
     ths->re += c.re;
     ths->im += c.im;
     return *ths;
 }
 
 inline Complex &
-_doami(Complex *ths, const Complex &c) {
+_doami(Complex *ths, const Complex &c)
+{
     ths->re -= c.re;
     ths->im -= c.re;
     return *ths;
 }
 
 inline Complex &
-_doaml(Complex *ths, const Complex &c) {
+_doaml(Complex *ths, const Complex &c)
+{
     double f = ths->re * c.re - ths->im * c.im;
     ths->im = ths->re * c.im + ths->im * c.re;
     ths->re = f;
@@ -69,82 +61,97 @@ _doaml(Complex *ths, const Complex &c) {
 
 // 实现成员操作符重载
 inline Complex &
-Complex::operator+=(const Complex &c) {
+Complex::operator+=(const Complex &c)
+{
     return _doapl(this, c);
 }
 
 inline Complex &
-Complex::operator-=(const Complex &c) {
+Complex::operator-=(const Complex &c)
+{
     return _doami(this, c);
 }
 
 inline Complex &
-Complex::operator*=(const Complex &c) {
+Complex::operator*=(const Complex &c)
+{
     return _doaml(this, c);
 }
 
 // 实现一些全局函数
 inline double
-real(const Complex &c) {
+real(const Complex &c)
+{
     return c.real();
 }
 
 inline double
-imag(const Complex &c) {
+imag(const Complex &c)
+{
     return c.imag();
 }
 
 // 实现全局操作符重载
 inline Complex
-operator+(const Complex &x, const Complex &y) {
+operator+(const Complex &x, const Complex &y)
+{
     return Complex(x.real() + y.real(), x.imag() + y.imag());
 }
 
 inline Complex
-operator-(const Complex &x, const Complex &y) {
+operator-(const Complex &x, const Complex &y)
+{
     return Complex(x.real() - y.real(), x.imag() - y.imag());
 }
 
 inline Complex
-operator+(const Complex &x, double y) {
+operator+(const Complex &x, double y)
+{
     return Complex(real(x) + y, imag(x));
 }
 
 inline Complex
-operator-(const Complex &x, double y) {
+operator-(const Complex &x, double y)
+{
     return Complex(real(x) - y, imag(x));
 }
 
 inline Complex
-operator+(double x, const Complex &y) {
+operator+(double x, const Complex &y)
+{
     return Complex(x + real(y), imag(y));
 }
 
 inline Complex
-operator-(double x, const Complex &y) {
+operator-(double x, const Complex &y)
+{
     return Complex(x - real(y), imag(y));
 }
 
 // 取反
 inline Complex
-operator-(const Complex &x) {
+operator-(const Complex &x)
+{
     return Complex(-real(x), -imag(x));
 }
 
 // 共轭
 inline Complex
-conj(const Complex &x) {
+conj(const Complex &x)
+{
     return Complex(real(x), -imag(x));
 }
 
 // 逻辑操作符重载
 inline bool
-operator==(const Complex &x, const Complex &y) {
+operator==(const Complex &x, const Complex &y)
+{
     return real(x) == real(y) && imag(x) == imag(y);
 }
 
 inline bool
-operator!=(const Complex &x, const Complex &y) {
+operator!=(const Complex &x, const Complex &y)
+{
     return real(x) != real(y) || imag(x) != imag(y);
 }
 
